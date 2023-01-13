9abbdfd157cc4b4266b8ed110a8a8208dbcc395b
Tom Österlund <tom.osterlund1@gmail.com>
2023-01-13 18:16:58 +0100

    add nuxt-vue-2 example

---
+++ nuxt-vue-2/components/PhormalContent.vue
@@ -0,0 +1,75 @@
+<template>
+	<div id="phormal"></div>
+</template>
+
+<script>
+import {Phormal, useLength, useEmail, useRequired} from '@phormal/core';
+
+export default {
+	name: 'PhormalContent',
+
+	data() {
+		return {
+			formFields: {
+				name: {
+					label: 'Name',
+					hooks: [useRequired(), useLength(3)]
+				},
+				email: {
+					label: 'Email',
+					hooks: [useRequired(), useEmail()]
+				},
+				newsletter: {
+					type: 'checkbox',
+					label: 'Newsletter',
+					value: true
+				},
+				deliveryMethod: {
+					type: 'radiogroup',
+					options: [
+						{ label: 'PayPal', value: 'paypal' },
+						{ label: 'Credit card', value: 'creditcard' },
+						{ label: 'Klarna', value: 'klarna' },
+					]
+				}
+			},
+			formConfig: {
+				theme: 'basic',
+				el: '#phormal',
+				language: 'en'
+			},
+			form: null
+		}
+	},
+
+	mounted() {
+		this.form = new Phormal(this.formFields, this.formConfig);
+	},
+
+	methods: {
+		validate() {
+			this.form.$validate();
+		},
+
+		getValues() {
+			return this.form.$values(); // { name: 'John Doe' }
+		}
+	},
+
+	watch: {
+		'form.name': {
+			handler: function (val) {
+				console.log('name changed to', val);
+			},
+		}
+	}
+};
+</script>
+
+<style>
+@import '@phormal/theme-basic/dist/index.css';
+
+#phormal {
+	font-family: sans-serif;
+}
+</style>
---
+++ nuxt-vue-2/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<PhormalContent />
+	</div>
+</template>
+
+<script>
+import PhormalContent from '~/components/PhormalContent';
+
+export default {
+	name: 'Index',
+
+	components: {
+		PhormalContent,
+	}
+};
+</script>
